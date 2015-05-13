3c4345db686d29b686ba7072ba46a9f8871926be
sebkit1984 <sebkit1984@gmail.com>
2015-05-13 10:56:09 +1000

    GameManager Class
    GameManager class manages time and score

---
+++ GameManager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+	int score;
+
+	float addScoreTime;
+	int nextScoreIncrement;
+	int timeScoreValue;
+
+	int multiplier;
+	float multiplierDecreaseTime;
+	float multiplierDecreaseTimeReset;
+
+	void Start () {
+		score = 0;
+
+		addScoreTime = 0;
+		nextScoreIncrement = 1;
+		timeScoreValue = 1;
+
+		multiplier = 1;
+		multiplierDecreaseTime = 0;
+		multiplierDecreaseTimeReset = 3;
+	}
+	void Update () {
+
+		addScoreTime += Time.deltaTime;
+		multiplierDecreaseTime += Time.deltaTime;
+
+		if (multiplierDecreaseTime >= multiplierDecreaseTimeReset) {
+			DecreaseMultiplier();
+			multiplierDecreaseTime = 0;
+		}
+
+		if (addScoreTime >= nextScoreIncrement) {
+			AddScore((int)(timeScoreValue * multiplier));
+			nextScoreIncrement += 1;
+		}
+	}
+	void AddScore(int a_scoreValue)
+	{
+		score += scoreValue;
+	}
+	void IncreaseMultiplier()
+	{
+		multiplier += 1;
+	}
+	void DecreaseMultiplier()
+	{
+		if (multiplier == 1) {
+			return;
+		}
+		multiplier -= 1;
+	}
+}
