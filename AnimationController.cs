6f3f6e900de7ea1370a5ab76d4ed657124b64026
Samuel Cole <colesamuel95@gmail.com>
2015-05-20 08:49:38 +1000

    Animation and Archer Raycast

---
+++ AnimationController.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent(typeof(SpriteRenderer))]
+public class AnimationController : MonoBehaviour 
+{
+	public bool attack = false;
+	
+	private SpriteRenderer myRenderer;
+
+	public Sprite[] movementSprites;
+	public Sprite[] actionSprites; 
+	
+	private int spriteNumber;	// what is the number of the sprite i'm vurrently seeing
+	private int spriteMax; 		// get the maximum number of sprites in the list
+	
+	private int actionMax;
+	private int movementMax;
+
+	private float switchTimer = 0.1f;
+
+
+	void Start () 
+	{
+		myRenderer = gameObject.GetComponent<SpriteRenderer>();
+
+		spriteNumber = 0;
+		actionMax = actionSprites.Length;
+		movementMax = movementSprites.Length;
+	}
+	
+	
+	void Update () 
+	{		
+		if (!attack)
+		{	
+			spriteMax = movementMax;
+			myRenderer.sprite = movementSprites[spriteNumber];
+		}
+		else
+		if (attack)
+		{
+			spriteMax = actionMax;
+			myRenderer.sprite = actionSprites[spriteNumber];
+		}
+		
+		Timer();
+
+	}
+	
+	void Timer()
+	{
+		switchTimer -= Time.deltaTime;
+		
+		if (switchTimer < 0)
+		{
+			// do stuff here
+			spriteNumber ++;
+			switchTimer = 0.1f;
+		}
+		
+		if (spriteNumber > spriteMax-1)
+		{
+			spriteNumber = 0;
+			attack = false;
+		}
+	}
+	
+	public void Action()
+	{
+		attack = true;
+	}
+	
+	public void StopAction()
+	{
+		//attack = false;
+		spriteNumber = 0;
+		attack = false;
+	}
+}
---
+++ ArcherRaycast.cs
@@ -0,0 +1,112 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent(typeof(LineRenderer))]			// Remember to set all AIpoints to "Ignore Raycast" layer
+public class ArcherRaycast : MonoBehaviour 
+{
+	private LineRenderer line;
+	
+	private RaycastHit hit;
+	
+	private bool timerDown;
+	private float timer;
+	
+	private EnemyAudio audio;
+	private GameObject parent;
+	private AnimationController anim;
+	private Color materialColor;
+
+	void Start () 
+	{	
+		parent = gameObject.transform.parent.gameObject;
+		audio = parent.GetComponent<EnemyAudio>();
+		anim = parent.GetComponentInChildren<AnimationController>();
+		
+		line = gameObject.GetComponent<LineRenderer>();
+		materialColor = line.material.color;
+		materialColor.a = 0.0f;
+		
+		line.useWorldSpace = true;
+		line.SetWidth(0.1f, 10.0f);
+	}
+	
+	
+	void Update () 
+	{
+		//Color materialColor = line.material.color;
+		if (timerDown)
+		{
+		
+			if (timer > 0)
+			{
+				timer -= Time.deltaTime;
+				line.SetWidth(0.1f, timer*10);
+				anim.Action();			// start attack animation
+			}
+			else
+			{
+				line.SetWidth(0.1f, 0.1f);
+				audio.Ranged(); // make a noise!!
+				Kill ();
+			}
+			
+			materialColor.a += 0.02f;		// make the line render
+		}
+		else
+		{
+			timer = 1.0f;
+			line.SetWidth(0.1f, 5.0f);
+			materialColor.a = 0.0f;			// stop the line render
+		}
+			
+		//materialColor.a = timer;
+		
+		if (materialColor.a < 0)		// lock the line render
+		{
+			materialColor.a = 0;
+		}
+		
+		if (materialColor.a > 1.0f)
+		{
+			materialColor.a = 1.0f;
+		}
+			
+		line.material.color = materialColor;
+		
+		Debug.Log ("Alpha  " + materialColor.a);
+		
+		
+		///////////////////////////////////////////////////////////////
+	
+		Ray	ray = new Ray(gameObject.transform.position, gameObject.transform.TransformDirection(Vector3.forward));
+		
+		if (Physics.Raycast(ray,out hit, 20))
+		{
+			timerDown = true;
+		}
+		else
+		{
+			timerDown = false;
+		}
+		
+		line.SetPosition (0, gameObject.transform.position);
+	
+		if (hit.collider != null)
+		{
+			line.SetPosition (1, ray.GetPoint(20));
+		}
+		else
+		{
+			line.SetPosition (1, ray.GetPoint(20));
+		}
+	}
+	
+	void Kill()
+	{
+		if (hit.collider != null)
+		{
+			GameObject.Destroy(hit.collider.gameObject);
+		}
+		timerDown = false;
+		anim.StopAction();	// stop the attack animation
+	}
+}
